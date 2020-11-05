2d4622109530e240845da7f0b242c97540d6b5aa
Maurizio <mau.moraca@gmail.com>
2020-11-05 15:06:07 +0100

    show image function updated

--- Utilities.py
+++ Utilities.py
@@ -2,15 +2,42 @@ from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 
-def read_image(path):
-    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+def read_image(path, color_scale = 'gray'):
+    color_scale_dictionary = {
+        "gray": cv2.IMREAD_GRAYSCALE,
+        "color": cv2.IMREAD_COLOR,
+        "anydepth": cv2.IMREAD_ANYDEPTH,
+        "anycolor": cv2.IMREAD_ANYCOLOR
+    }
+    scale = color_scale_dictionary.get(color_scale)
+    if(scale == None):
+        scale = cv2.IMREAD_GRAYSCALE
+
+    image = cv2.imread(path, scale)
     return image
 
 
 def show_image(image):
-    plt.imshow(image, cmap='gray', interpolation='bicubic', vmin=0, vmax=255)
-    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-    plt.show()
+    image = resize_with_aspect_ratio(image, 600)
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def resize_with_aspect_ratio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
 
 def show_histogram(histogram, x_label="", y_label=""):
     plt.plot(np.arange(histogram.size), histogram)
--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from Utilities import show_histogram
 from ImageEnhancement import make_image_negative
 from ImageEnhancement import histogram_of_grayscale
 import numpy as np
-path= 'images/1577x1365.png'
-image = read_image(path)
-N = image.shape[0] * image.shape[1]
-"""show_image(image)
+#path= 'images/1577x1365.png'
+path= 'images/745x419.jpg'
+image = read_image(path, "gray")
 
-show_image(make_image_negative(image))"""
-histogram = histogram_of_grayscale(image)
+N = image.shape[0] * image.shape[1]
+#show_image(image)
+
+show_image(make_image_negative(image))
+"""histogram = histogram_of_grayscale(image)
 print(histogram)
-show_histogram(histogram/N, "Pixel intensity", "Pixel frequency")
+show_histogram(histogram/N, "Pixel intensity", "Pixel frequency")"""
